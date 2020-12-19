66f8186e0c6015dbed5a491b435598e4fa4cdd68
dadabit <74517890+dadabit@users.noreply.github.com>
2020-12-19 20:10:05 +0800

    Update palindromes.py

--- palindromes.py
+++ palindromes.py
@@ -1,17 +1,20 @@
 
-word = ['r', 'a', 'c', 'e', 'c', 'a', 'r']
+word = ['r', 'a', 'c', 'e', '3', 'a', 'r']
 
 last = len(word) - 1
 first = 0
+is_palindrome = True
 
 count = len(word) / 2
 
-while first <= count:
+while first <= count and is_palindrome:
     
     if word[last] == word[first]:
         print(word[last] + " and " + word[first] + " is the same!")
+    else:
+        is_palindrome = False
     
-    first += 2
+    first += 1
     last -= 1
     
     
