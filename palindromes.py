536470590bfe6db6e758de439591abfe65faca72
dadabit <74517890+dadabit@users.noreply.github.com>
2020-12-19 19:58:38 +0800

    Add files via upload

---
+++ palindromes.py
@@ -0,0 +1,19 @@
+
+word = ['r', 'a', 'c', 'e', 'c', 'a', 'r']
+
+last = len(word) - 1
+first = 0
+
+count = len(word) / 2
+
+while first <= count:
+    
+    if word[last] == word[first]:
+        print(word[last] + " and " + word[first] + " is the same!")
+    
+    first += 1
+    last -= 1
+    
+    
+
+
