b4fe2236cc1c03455123ba5a6c63395b1a536923
dadabit <74517890+dadabit@users.noreply.github.com>
2020-12-19 23:18:21 +0800

    Update palindromes.py
    uuju

--- palindromes.py
+++ palindromes.py
@@ -1,27 +1,32 @@
+word_File = open('words.txt', 'r') #open the file
 
-word = ['r', 'a', 'c', 'e', '3', 'a', 'r']
+def check_palindrome(p_word): #function that check the word if it is a palindrome
 
-last = len(word) - 1
-first = 0
-is_palindrome = True
-is_palindrome_times = 0
+    last = len(p_word) - 1
+    first = 0
+    is_palindrome_times = 0
 
-count = len(word) / 2
+    count = len(p_word) / 2
 
-while first <= count and is_palindrome:
+    while first <= count:
     
-    if word[last] == word[first]:
-        is_palindrome_times += 1
-    else:
-        print("this word is not a palindrome")
-        is_palindrome = False
+        if p_word[last] == p_word[first]:
+            is_palindrome_times += 1
+        else:
+            print("this word is not a palindrome")
+            first = count + 1
     
-    first += 1
-    last -= 1
+        first += 1
+        last -= 1
     
-    if is_palindrome_times >= count:
-        print("this word is a palindromes")
+        if is_palindrome_times >= count:
+            print("this word is a palindromes")
+            
+for word in word_File:
+    words = list(word)
+    check_palindrome(words)
     
     
 
 
+word_File.close()
