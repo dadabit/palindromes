7f594d2f007caa9f376a90553f2248917013ce14
dadabit <74517890+dadabit@users.noreply.github.com>
2020-12-19 23:39:14 +0800

    Update palindromes.py

--- palindromes.py
+++ palindromes.py
@@ -13,14 +13,14 @@ def check_palindrome(p_word): #function that check the word if it is a palindrom
         if p_word[last] == p_word[first]:
             is_palindrome_times += 1
         else:
-            print("this word is not a palindrome")
+            print("this is not a palindrome")
             first = count + 1
     
         first += 1
         last -= 1
     
         if is_palindrome_times >= count:
-            print("this word is a palindromes")
+            print("this is a palindromes")
             
 for word in word_File:
     words = list(word.strip())
